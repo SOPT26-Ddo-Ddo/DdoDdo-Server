5e56fd88a9037a2a3e709c0449419b4dce966715
kimminji122258 <kimminji122258@gmail.com>
2020-06-07 03:52:43 +0900

    msg

---
+++ server/models/group.js
@@ -0,0 +1,54 @@
+const pool = require('../modules/pool');
+const table = 'UserGroup';
+
+const group = {
+
+    // 속한 그룹들 전체 조회
+    getGroupAllRead: async (userIdx) => {
+        const query = `SELECT * FROM Group AS G 
+        INNER JOIN ( SELECT groupIdx FROM UserGroup WHERE userIdx = ${userIdx} ) 
+        AS R ON G.groupIdx = R.groupIdx;`;
+
+        try {
+            const result = await pool.queryParam(query);
+            return result;
+        } catch (err) {
+            console.log('GroupAllRead ERROR : ', err);
+            throw err;
+        }
+    },
+
+    // 특정 그룹 정보 조회
+    getGroupInfoRead: async (groupIdx) => {
+        const query = `SELECT * FROM Group WHERE groupIdx=${groupIdx}`;
+
+        try {
+            const result = await pool.queryParam(query);
+            return result;
+        } catch (err) {
+            console.log('GroupInfoRead ERROR : ', err);
+            throw err;
+        }
+    },
+
+    // 특정 그룹 사용자들 조회
+    getGroupUserRead: async (groupIdx) => {
+        const query = `SELECT * FROM User AS U
+        INNER JOIN ( SELECT userIdx FROM UserGroup WHERE groupIdx = ${groupIdx} ) 
+        AS R ON U.userIdx = R.userIdx`;
+
+        try {
+
+            const result = await pool.queryParam(query2);
+            return result;
+        } catch (err) {
+            console.log('GroupUserRead ERROR : ', err);
+            throw err;
+        }
+    }
+
+
+}
+
+
+module.exports = group;
--- server/routes/group.js
+++ server/routes/group.js
@@ -1,9 +1,49 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+let util = require('../modules/util');
+let statusCode = require('../modules/statusCode');
+let resMessage = require('../modules/responseMessage');
+let GroupModel = require('../models/group');
+
+/* 특정 그룹 정보 조회 */
+router.get('/:groupIdx', async (req, res) => {
+  const groupIdx = req.params.groupIdx;
+
+  const groupResult = await GroupModel.getGroupInfoRead(groupIdx);
+  const userResult = await GroupModel.getGroupUserRead(groupIdx);
+
+  // 해당 그룹 존재하지 않음
+  if (result.length == 0) {
+    res.status(statusCode.BAD_REQUEST)
+      .send(util.fail(statusCode.BAD_REQUEST, resMessage.GROUP_FAIL));
+  }
+
+  res.status(statusCode.OK)
+    .send(util.success(statusCode.OK, resMessage.GROUP_SUCCESS, {
+      groupInfo: groupResult,
+      groupUser: userResult
+    }));
 });
+
+// /* 마니또 알고리즘 수행 */
+// router.get('/:groupIdx/user/:userIdx', function (req, res, next) {
+//   const groupIdx = req.params.groupIdx;
+
+//   const groupResult = await GroupModel.getGroupInfoRead(groupIdx);
+//   const userResult = await GroupModel.getGroupUserRead(groupIdx);
+
+//   // 해당 그룹 존재하지 않음
+//   if (result.length == 0) {
+//     res.status(statusCode.BAD_REQUEST)
+//       .send(util.fail(statusCode.BAD_REQUEST, resMessage.GROUP_FAIL));
+//   }
+
+//   res.status(statusCode.OK)
+//     .send(util.success(statusCode.OK, resMessage.GROUP_SUCCESS, {
+//       groupInfo: groupResult,
+//       groupUser: userResult
+//     }));
+// });
 
 module.exports = router;
