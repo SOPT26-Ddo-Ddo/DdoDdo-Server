be65a9ec7d34974f117af033cebc1b7fd0b8aef3
KimDoubleB <tree9295@gmail.com>
2020-06-10 10:26:38 +0900

    Add User (sign up, sign in, read profile)

--- server/models/user.js
+++ server/models/user.js
@@ -3,16 +3,54 @@ const encrypt = require('../modules/encryption');
 const table = 'User';
 
 const user = {
-    getUserById: async (id) => {
+    signup: async (id, pwd, salt, name, gender, profileMsg) => {
+        const fields = 'id, pwd, salt, name, gender, profileMsg';
+        const questions = '?,?,?,?,?,?';
+        const values = [id, pwd, salt, name, gender, profileMsg];
+        const query = `INSERT INTO ${table}(${fields}) VALUES(${questions})`;
+        try {
+            const result = await pool.queryParamArr(query, values);
+            console.log('Sign up - result: ', result);
+            const insertId = result.insertId;
+            return insertId;
+        } catch (err) {
+            if (err.errno == 1062) {
+                console.log('signup ERROR : ', err.errno, err.code);
+                return -1;
+            }
+            console.log('signup ERROR: ', err);
+            throw err;
+        }
+    },
+    checkUser: async (id) => {
         const query = `SELECT * FROM ${table} WHERE id = "${id}"`;
         try {
-            return await pool.queryParam(query);
+            const result = await pool.queryParam(query);
+            if (result.length === 0) {
+                return false;
+            } else
+                return true;
         } catch (err) {
             throw err;
         }
     },
-    getUserByIdx: async (userIdx) => {
-        const query = `SELECT * FROM ${table} WHERE userIdx = "${userIdx}"`;
+    signin: async (id, pwd) => {
+        const query = `SELECT * FROM ${table} WHERE id = "${id}"`;
+        try {
+            const result = await pool.queryParam(query);
+            if (result[0].pwd !== encrypt.encryption(pwd, result[0].salt)) {
+                // 비밀번호가 일치하지 않는가?
+                return -1;
+            }
+            console.log(result[0])
+            return result[0].userIdx;
+
+        } catch (err) {
+            throw err;
+        }
+    },
+    getUserById: async (id) => {
+        const query = `SELECT * FROM ${table} WHERE id = "${id}"`;
         try {
             return await pool.queryParam(query);
         } catch (err) {
--- server/routes/token.js
+++ server/routes/token.js
@@ -6,19 +6,20 @@ const util = require('../modules/util')
 const statusCode = require('../modules/statusCode');
 const resMessage = require('../modules/responseMessage');
 const UserModel = require('../models/user');
-/* GET 메인화면 인기사진 리스트 불러오기 */
+
 router.get('/', async (req, res) => {
     const id = 'bobae'; // specific id
     const user = await UserModel.getUserById(id);
-
     if (user[0] === undefined) {
         return res.status(statusCode.BAD_REQUEST)
             .send(util.fail(statusCode.BAD_REQUEST, resMessage.NO_USER));
     }
-    const {token,_} = await jwt.sign(user[0]);
 
+    const {
+        token,
+        _
+    } = await jwt.sign(user[0]);
 
-    // 성공 - login success와 함께 user Id 반환
     return res.status(statusCode.OK)
         .send(util.success(statusCode.OK, resMessage.LOGIN_SUCCESS, {
             accessToken: token
